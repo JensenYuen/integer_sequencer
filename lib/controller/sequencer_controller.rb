7bf595d864cca5960f09e523ea08fc1fb5aee538
jensen yuen <jensenyuen@outlook.com>
2022-01-28 23:24:25 +0800

    added more logic

--- lib/controller/sequencer_controller.rb
+++ lib/controller/sequencer_controller.rb
@@ -4,6 +4,7 @@ require 'pry-byebug'
 
 # logic of the sequencer is stored here
 class SequencerController
+
   def initialize(user_input)
     @order = 0
     @user_input = user_input
@@ -11,6 +12,8 @@ class SequencerController
   end
 
   def sequencer
+    return unless valid_arr_size? || valid_integer_range? || valid_order_size?
+
     @order = 1 unless valid_order?(@order)
     @order.times do
       @sequence = subtrator
@@ -44,8 +47,25 @@ class SequencerController
     true
   end
 
+  # Extra consideration to prevent user from running the  program if invalid inputs
   # Extra methods below to check for
   # if the input contains between 1 to 20 integers (inclusive)
+  def valid_arr_size?
+    arr_size = @user_input.size
+    arr_size < 1 || arr_size > 20
+  end
+
   # if the integer are between -200 and 200, inclusive.
+  def valid_integer_range?
+    result = false
+    @sequence.each do |number|
+      result = true if number < -200 || number > 200
+    end
+    result
+  end
+
   # if n is between 0 and K-1, inclusive, where K is the number of elements in the input
+  def valid_order_size?
+    @order.negative? || @order < @sequence.size
+  end
 end
--- lib/interface.rb
+++ lib/interface.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: false
 
 require_relative './controller/sequencer_controller'
+require 'pry-byebug'
 
 running = true
 
 while running
+  puts `clear`
   puts 'Enter a sequence of integers separated by space, then a comma, and the order n.'
+  puts 'Please input between 1 to 20 integers (inclusive)'
+  puts 'Please enter integer that are between -200 and 200, inclusive.'
+  puts 'Please enter n such that it is no larger than the number of integers entered'
   puts 'eg: 5 6 3 9 -1,2 '
   print "\nYour sequence :"
   user_input = gets.chomp
   number_sequencer = SequencerController.new(user_input)
   answer = number_sequencer.sequencer
-  print 'sequenced number :'
-  answer.each do |number|
-    print " #{number} "
+  unless answer.nil?
+    print 'sequenced number :'
+    answer.each do |number|
+      print " #{number} "
+    end
+  else
+    puts 'Your input sequence is incorrect'
   end
   running = false
 end
